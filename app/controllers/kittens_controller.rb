a936815ea0d56ca1a5125e69be1c261e0b3fcfb6
Dana Yatsuta <danayatsuta@gmail.com>
2021-03-10 10:23:46 +0300

    Added JSON support for kittens index action

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -20,6 +20,11 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens}
+    end
   end
 
   def edit
